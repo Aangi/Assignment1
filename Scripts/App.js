48d6734c7889c96ce2512c215616142a30a9f010
Aangi <aashah.6198@gmail.com>
2016-06-03 17:35:44 -0400

    Updated App.js

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,27 +1,33 @@
 /* main javascript file  */
-
+//IIFE - Immediately Invoked Function Expression
 (function(){
     
     "use strict";
 // code codes in here 
-console.log("App started...");
 
-var firstParagraphTag = document.getElementById("firstParagraph");
+// define an array of HTML elements
+     var paragraphElements = [];
+     
+     paragraphElements[0] = document.getElementById("aboutPara");
+    paragraphElements[1] = document.getElementById("projectsPara");
+     
+     // define your paragraphs array;
+     var paragraphs = [];
+     
+     // data for my pages
+     paragraphs[0] = "This is my first paragraph. It is only visible on the first page. This next sentence is to prove that this really works!!!";
+     paragraphs[1] = "This is my second paragraph. It is only visible on the second page";
 
-var firstParagraph = "It is my first paragraph";
-  
-
-//declare named function
-function writeTofirstParagraph() {
-    
-        //create a reference to a p element with an id of "firstParagraph"
-        firstParagraphTag.textContent = firstParagraph;
-      }
-      writeTofirstParagraph();
-    
-    
-    
-    
-    
-    
-})();
+     
+     // second way to define an array
+     //var paragraphs = new Array();
+     
+     // check to see if paragraph one exists
+     for (var index = 0; index < paragraphElements.length; index++) {
+         if(paragraphElements[index]) {
+          paragraphElements[index].textContent = paragraphs[index];
+         }
+     }
+      
+      
+  })(); 
