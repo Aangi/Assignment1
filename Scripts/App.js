f4ab167ab8bb521766ada332b6c486c22c166424
Aangi <aashah.6198@gmail.com>
2016-06-03 14:50:05 -0400

    Initial Commit

---
+++ Scripts/App.js
@@ -0,0 +1,21 @@
+/* main javascript file  */
+
+(function(){
+    
+    "use strict";
+// code codes in here 
+console.log("App started...");
+function writeTofirstParagraph() {
+        //create a reference to a p element with an id of "firstParagraph"
+
+        var firstParagraph = document.getElementById("firstParagraph");
+   
+        firstParagraph.textContent = "It is my first paragraph"
+    }
+    
+    
+    
+    
+    
+    
+})();
