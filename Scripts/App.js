fcca49a504704e8696678a52d5dd390259f5f96a
Aangi <aashah.6198@gmail.com>
2016-06-03 18:41:26 -0400

    Updated About Me Page

--- Scripts/App.js
+++ Scripts/App.js
@@ -8,16 +8,16 @@
 // define an array of HTML elements
      var paragraphElements = [];
      
-     paragraphElements[0] = document.getElementById("aboutPara");
+    paragraphElements[0] = document.getElementById("aboutPara");
     paragraphElements[1] = document.getElementById("projectsPara");
-     
+    paragraphElements[2] = document.getElementById("contactPara");
      // define your paragraphs array;
      var paragraphs = [];
      
      // data for my pages
-     paragraphs[0] = "This is my first paragraph. It is only visible on the first page. This next sentence is to prove that this really works!!!";
+     paragraphs[0] = "My name is <strong>Aayushi Shah</strong>. I am from India. I am studying <strong>Software Engineering Technology</strong> at Centennial College. I want to become an outstanding Software Engineer after completing my course. I want to get a degree from a renowned university after graduating from this course. Other than my field, I like mathematics subject the most.";
      paragraphs[1] = "This is my second paragraph. It is only visible on the second page";
-
+     paragraphs[2] = ""
      
      // second way to define an array
      //var paragraphs = new Array();
@@ -25,7 +25,7 @@
      // check to see if paragraph one exists
      for (var index = 0; index < paragraphElements.length; index++) {
          if(paragraphElements[index]) {
-          paragraphElements[index].textContent = paragraphs[index];
+          paragraphElements[index].innerHTML = paragraphs[index];
          }
      }
       
