3cd43ee5b9566e9ed1bb8c7147a51cf689959318
Aangi <aashah.6198@gmail.com>
2016-06-03 16:56:40 -0400

    Applied bootstrap, font awesome and navbar

--- Scripts/App.js
+++ Scripts/App.js
@@ -5,13 +5,19 @@
     "use strict";
 // code codes in here 
 console.log("App started...");
-function writeTofirstParagraph() {
-        //create a reference to a p element with an id of "firstParagraph"
 
-        var firstParagraph = document.getElementById("firstParagraph");
-   
-        firstParagraph.textContent = "It is my first paragraph"
-    }
+var firstParagraphTag = document.getElementById("firstParagraph");
+
+var firstParagraph = "It is my first paragraph";
+  
+
+//declare named function
+function writeTofirstParagraph() {
+    
+        //create a reference to a p element with an id of "firstParagraph"
+        firstParagraphTag.textContent = firstParagraph;
+      }
+      writeTofirstParagraph();
     
     
     
